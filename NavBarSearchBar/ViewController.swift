eaba49bf84d4be2a4464ae7325fa8c447dc8d4d0
fingent <fingent@fingents-mini-5.ftsindia.in>
2016-02-16 15:58:17 +0530

    nav bar

--- NavBarSearchBar/ViewController.swift
+++ NavBarSearchBar/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController,UINavigationBarDelegate {
 	var rightSearchBarButtonItem: UIBarButtonItem?
 
 	override func viewDidLoad() {
+		
 		super.viewDidLoad()
 	self.navigationController?.navigationBarHidden =  false
 		
